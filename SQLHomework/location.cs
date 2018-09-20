4d3b32c731994bc2144b40738989ba1811dee3e5
BK <brian_j_king@me.com>
2018-09-20 16:59:04 -0500

    Added location.cs and Modified .gitignore

---
+++ SQLHomework/location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLHomework
+{
+    public class location
+    {
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+        public double CostRate { get; set; }
+        public decimal Availability { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}

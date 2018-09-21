b025e2e0cc4e367a523407b57983ee6611107b2f
BK <brian_j_king@me.com>
2018-09-20 20:46:44 -0500

    CRUD Update Done BK

--- SQLHomework/Program.cs
+++ SQLHomework/Program.cs
@@ -8,9 +8,31 @@ namespace SQLHomework
         {
             LocationRepository location = new LocationRepository();
 
-            Console.WriteLine("Creating new Location enrty...");
+            //Console.WriteLine("Creating new Location enrty...");
 
-            location.CreateLocation("Beard Oil", 1.50, 15.00m, DateTime.Now);
+            //location.CreateLocation("Beard Oil", 1.50, 15.00m, DateTime.Now);
+
+            //Console.WriteLine("Transaction Completed!!!");
+
+            Location l = new Location();
+            l.LocationID = 61;
+            l.Name = "Kingz of Kingz Beard Oil";
+            l.CostRate = 2.50;
+            l.Availability = 20.50m;
+            l.ModifiedDate = DateTime.Now;
+
+            Console.WriteLine("Updating Location Table.....");
+            //Udate All fields
+            //location.UpdateLocation(l);
+
+            //Update Name
+            //location.UpdateLocation("Kingz of Kingz Beard Club Beard Oil",61,DateTime.Now);
+
+            //Update CostRate
+            //location.UpdateLocation(3.15, 61, DateTime.Now);
+
+            //Update Availability
+            location.UpdateLocation(25.50m, 61, DateTime.Now);
 
             Console.WriteLine("Transaction Completed!!!");
 
--- SQLHomework/locationrepository.cs
+++ SQLHomework/locationrepository.cs
@@ -30,13 +30,113 @@ namespace SQLHomework
                 cmd.Parameters.AddWithValue("availability", a);
                 cmd.Parameters.AddWithValue("modifiedate", m);
                 cmd.ExecuteNonQuery();
-
             }
+        }
+
+        /// <summary>
+        /// UdateLocation method updates the CostRate field in the location table
+        /// takes Location object parameters 
+        /// And updates a Location within our database with that all fields.
+        /// </summary>
+        public void UpdateLocation(Location l)
+        {
+            var conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = "UPDATE location SET Name = @name, CostRate = @costrate, " +
+                                  "Availability = @availability,ModifiedDate =  @modifiedate " +
+                                  "WHERE LocationID = @locationID";
+                cmd.Parameters.AddWithValue("name", l.Name);
+                cmd.Parameters.AddWithValue("costrate", l.CostRate);
+                cmd.Parameters.AddWithValue("availability", l.Availability);
+                cmd.Parameters.AddWithValue("modifiedate", l.ModifiedDate);
+                cmd.Parameters.AddWithValue("LocationID", l.LocationID);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        /// <summary>
+        /// UdateLocation method updates the Name  field in the location table
+        /// takes three parameters  (n = Name,  l = LocationID, d = ModifiedDate)
+        /// And updates a Location within our database with that a new Name and ModifiedDate.
+        /// </summary>
+        public void UpdateLocation(string n, int l, DateTime m)
+        {
+            var conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = "UPDATE location SET Name = @name, ModifiedDate =  @modifiedate " +
+                                  "WHERE LocationID = @locationID";
+                cmd.Parameters.AddWithValue("name", n);
+                cmd.Parameters.AddWithValue("modifiedate", m);
+                cmd.Parameters.AddWithValue("LocationID", l);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        /// <summary>
+        /// UdateLocation method updates the CostRate field in the location table
+        /// takes three parameters  (c = CostRate,  l = LocationID, d = ModifiedDate)
+        /// And updates a Location within our database with that new CostRate and ModifiedDate.
+        /// </summary>
+        public void UpdateLocation(double c, int l, DateTime m)
+        {
+            var conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = "UPDATE location SET CostRate = @costrate, ModifiedDate =  @modifiedate " +
+                                  "WHERE LocationID = @locationID";
+                cmd.Parameters.AddWithValue("costrate", c);
+                cmd.Parameters.AddWithValue("modifiedate", m);
+                cmd.Parameters.AddWithValue("LocationID", l);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        /// <summary>
+        /// UdateLocation method updates the CostRate field in the location table
+        /// takes three parameters  (c = CostRate,  l = LocationID, d = ModifiedDate)
+        /// And update a Location within our database with that new Availability and ModifiedDate.
+        /// </summary>
+        public void UpdateLocation(decimal a, int l, DateTime m)
+        {
+            var conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = "UPDATE location SET Availability = @availability, ModifiedDate =  @modifiedate " +
+                                  "WHERE LocationID = @locationID";
+                cmd.Parameters.AddWithValue("availability", a);
+                cmd.Parameters.AddWithValue("modifiedate", m);
+                cmd.Parameters.AddWithValue("LocationID", l);
+                cmd.ExecuteNonQuery();
+            }
+
         }
 
 
 
-
-
-    }       
+    }
 }
